d0e3f16a9ccced817cd2b22d470cf044313aacc3
agustinterruzzi <agustinterruzzi@676dea2c-8a08-0410-817f-c220af3ca6b2>
2012-06-11 14:35:35 +0000

    Sexto retoque para arreglo de encoding

--- plugins/reports/MultiGeneratorPaperReport/ReportClass/PaperReportTXT.inc.php
+++ plugins/reports/MultiGeneratorPaperReport/ReportClass/PaperReportTXT.inc.php
@@ -77,7 +77,7 @@ class PaperReportTXT extends PaperReportHandler{
 			$output= $this->addField($output,$paper->getTrackTitle());
 			
 		}
-		$output= $this->addField($output,utf8_encode($paper->getTitle($locale)));
+		$output= $this->addField($output,html_entity_decode(strip_tags($paper->getTitle($locale)), ENT_QUOTES));
 		$output= $this->addField($output,$paper->getAbstract($locale));
 		$authors= $this->formatAuthors($paper->getAuthors($locale));
 		$output= $this->addField($output,$authors);
