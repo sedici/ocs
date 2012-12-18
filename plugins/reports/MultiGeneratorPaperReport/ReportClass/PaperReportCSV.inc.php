a291fcc042ab3784fd81d0d59c34cad9dfe763bb
agustinterruzzi <agustinterruzzi@676dea2c-8a08-0410-817f-c220af3ca6b2>
2012-12-18 13:43:43 +0000

    Arreglo de errores de encoding, estandarizado todo a utf-8

--- plugins/reports/MultiGeneratorPaperReport/ReportClass/PaperReportCSV.inc.php
+++ plugins/reports/MultiGeneratorPaperReport/ReportClass/PaperReportCSV.inc.php
@@ -95,7 +95,7 @@ class PaperReportCSV extends PaperReportHandler{
 	function processRecord(&$paper,&$locale){
 		$output= $this->newOutput();
 		$this->addField($output,'track',$paper->getTrackTitle());
-	    $this->addField($output,'title',$paper->getTitle($locale));
+	    $this->addField($output,'title',utf8_encode($paper->getTitle($locale)));
 	    $this->addField($output,'abstract',utf8_encode($paper->getAbstract($locale)));
 		$this->maxAuthors=count($paper->getAuthors($locale))>$this->maxAuthors?count($paper->getAuthors($locale)):$this->maxAuthors;
 	    $this->putAuthors($paper->getAuthors($locale),$output);
