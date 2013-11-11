cae894d2ff6225d863dc4e4784ac66f461775b33
agustinterruzzi <agustinterruzzi@676dea2c-8a08-0410-817f-c220af3ca6b2>
2013-11-11 13:35:34 +0000

    Arreglo para español del plugin para cargar imágenes

---
+++ lib/pkp/lib/tinymce/jscripts/tiny_mce/plugins/jbimages/ci/application/language/spanish/jbstrings_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['jb_blankpage_message'] = "El proceso de carga no ha comenzado, o ha comenzado, pero todavia no ha terminado, o su navegador no ha podido acceder al servidor.";
+$lang['jb_delete'] = "Borrar";
+$lang['jb_no_files'] = "No ha cargado ningun archivo";
+$lang['jb_go_back'] = "Volver";
+$lang['jb_list_disabled'] = "Este server ha deshabilitado el listado de archivos";
+$lang['jb_uploaded_images'] = "Subir imágenes";
+
+/* End of file jbstrings_lang.php */
+/* Location: ./application/language/spanish/jbstrings_lang.php */
